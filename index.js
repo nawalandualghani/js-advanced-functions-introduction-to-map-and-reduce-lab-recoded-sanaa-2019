2bdc94601db456e7bc809d418e9d28ab7246e374
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-21 12:30:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function mapToNegativize(arr) {
   return r
 }
 
-function mapToNoChange(arr {
+function mapToNoChange(src) {
   let r = []
-  for (let i = 0; i < arr.length; i++ ) {
-    r.push(arr[i])
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i])
   }
   return r
 }
